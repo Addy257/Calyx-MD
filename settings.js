a65fbdd7bf598912cad3cfba65daffbe956edc46
Addy257 <muleyaaddy5@gmail.com>
2025-05-23 19:48:19 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Calyx',
+  packname: 'DEE',
   author: 'Bot',
-  botName: "Calyx-MD",
-  botOwner: 'Drey', // Your name
-  ownerNumber: '263716558758', //Your number
+  botName: "Dee MD",
+  botOwner: 'boss dee', // Your name
+  ownerNumber: '263779678867', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
